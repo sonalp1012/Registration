f225a3e47bc6bd22e22f9ad77c9aea6baa11453c
sonalp1012 <49842529+sonalp1012@users.noreply.github.com>
2020-01-24 14:54:32 -0500

    Commit 1 2:54

--- src/test/java/com/automation/openmrs/tests/HomeTest.java
+++ src/test/java/com/automation/openmrs/tests/HomeTest.java
@@ -3,8 +3,6 @@ import com.automation.openmrs.base.TestBase;
 import com.automation.openmrs.po.HomePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 @Test
